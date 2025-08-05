1d210d4c12c28c3f89d26b5f2ee4360b246ceddc
Eduardo Melo <nascimentomeloedu@gmail.com>
2025-08-05 14:53:01 -0300

    feat(customer): implement yup validation pattern for customer entity
    - Add yup validation library and type definitions
    - Create validator interface for entity validation
    - Implement CustomerYupValidator with yup schema validation
    - Add CustomerValidatorFactory for validator creation
    - Update Customer entity to use factory-based validation
    - Make notification property public in Entity abstract class
    - Fix notification error message ordering in NotificationError
    
    This implementation follows the validator pattern to separate validation
    logic from domain entities and provides a flexible validation system
    using yup library for schema-based validation.

--- src/domain/@shared/notification/notification.error.ts
+++ src/domain/@shared/notification/notification.error.ts
@@ -2,6 +2,6 @@ import { NotificationErrorProps } from "./notification";
 
 export default class NotificationError extends Error {
     constructor(public errors: NotificationErrorProps[]) {
-        super(errors.map(error => `${error.context}: ${error.message}`).reverse().join(", "));
+        super(errors.map(error => `${error.context}: ${error.message}`).join(", "));
     }
 }
--- src/domain/customer/entity/customer.ts
+++ src/domain/customer/entity/customer.ts
@@ -1,5 +1,6 @@
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
+import CustomerValidatorFactory from "../factory/customer.validator.factory";
 import Address from "../value-object/address";
 
 export default class Customer extends Entity {
@@ -59,12 +60,7 @@ export default class Customer extends Entity {
 	}
 
 	validate() {
-		if (this._name.length === 0) {
-			this.notification.addError({ context: "customer", message: "Name is required" });
-		}
-		if (this.id.length === 0) {
-			this.notification.addError({ context: "customer", message: "Id is required" });
-		}
+		CustomerValidatorFactory.create().validate(this);
 		if (this.notification.hasErrors()) {
 			throw new NotificationError(this.notification.getErrors());
 		}
